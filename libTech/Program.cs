a2b118db4610259223bc7b35df28f710b4c0972a
Saša Barišić <cartman300@net.hr>
2018-01-07 14:41:27 +0100

    Project files

---
+++ libTech/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libTech {
+	class Program {
+		static void Main(string[] args) {
+			Console.Title = "libTech";
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
