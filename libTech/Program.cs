a9fb547164d0e84bb577abc861fb9deec5bbff2f
Saša Barišić <cartman300@net.hr>
2018-01-07 16:30:41 +0100

    Ultraviolet
    https://github.com/tlgkccampbell/ultraviolet

--- libTech/Program.cs
+++ libTech/Program.cs
@@ -4,13 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Ultraviolet;
+using Ultraviolet.Content;
+using Ultraviolet.Core;
+using Ultraviolet.Core.Text;
+using Ultraviolet.Graphics.Graphics2D;
+using Ultraviolet.Graphics.Graphics2D.Text;
+using Ultraviolet.OpenGL;
+using Ultraviolet.Platform;
+
 namespace libTech {
-	class Program {
+	public class Program : UltravioletApplication {
 		static void Main(string[] args) {
 			Console.Title = "libTech";
 
-			Console.WriteLine("Done!");
-			Console.ReadLine();
+			using (Program P = new Program()) {
+				P.Run();
+			}
+		}
+
+		public Program() : base("Carpmanium", "libTech") {
+
+		}
+
+		protected override UltravioletContext OnCreatingUltravioletContext() {
+			OpenGLUltravioletConfiguration Cfg = new OpenGLUltravioletConfiguration();
+
+#if DEBUG
+			Cfg.Debug = true;
+			Cfg.DebugLevels = DebugLevels.All;
+			Cfg.DebugCallback = (UV, Lvl, Msg) => {
+				Console.WriteLine(Msg);
+			};
+#endif
+
+
+			return new OpenGLUltravioletContext(this, Cfg);
+		}
+
+		protected override void OnInitialized() {
+			base.OnInitialized();
+
+			IUltravioletPlatform Platform = Ultraviolet.GetPlatform();
+			IUltravioletWindow Window = Platform.Windows.FirstOrDefault();
+
+			if (Window != null) 
+				Window.Caption = "libTech";	
+		}
+
+		protected override void OnDrawing(UltravioletTime time) {
+			base.OnDrawing(time);
+
+			IUltravioletGraphics Gfx = Ultraviolet.GetGraphics();
+			Gfx.Clear(Color.DarkSlateGray);
 		}
 	}
 }
