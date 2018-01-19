9385943b0960f98d0a78da2e8e3414db5008c1fc
Saša Barišić <cartman300@net.hr>
2018-01-19 09:06:56 +0100

    Fixed GUI rendertarget flickering

--- libTech/Program.cs
+++ libTech/Program.cs
@@ -24,162 +24,13 @@ using System.IO;
 using NuklearDotNet;
 using System.Runtime.InteropServices;
 using Ultraviolet.Input;
+using libTech.Renderer;
 
 namespace libTech {
-	internal unsafe class RenderDevice : NuklearDeviceTex<Texture2D> {
-		Engine Engine;
-
-		VertexDeclaration NuklearVertexDecl;
-
-		IUltravioletWindow Wind;
-		IUltravioletGraphics Graphics;
-		RenderTarget2D RenderTarget;
-		RenderBuffer2D ColorBuffer;
-		SpriteBatch Batch;
-
-		SamplerState Sampling;
-		DepthStencilState DepthStencil;
-		BlendState Blending;
-		RasterizerState Rasterizing;
-		Viewport VPort;
-
-		BasicEffect Effect;
-		GeometryStream GeomStream;
-		DynamicVertexBuffer VertexBuffer;
-		DynamicIndexBuffer IndexBuffer;
-
-		public RenderDevice(Engine Engine) {
-			this.Engine = Engine;
-
-			NuklearVertexDecl = new VertexDeclaration(new VertexElement[] {
-					new VertexElement((int)Marshal.OffsetOf(typeof(NkVertex), nameof(NkVertex.Position)), VertexFormat.Vector2, VertexUsage.Position, 0),
-					new VertexElement((int)Marshal.OffsetOf(typeof(NkVertex), nameof(NkVertex.UV)), VertexFormat.Vector2, VertexUsage.TextureCoordinate, 0),
-					new VertexElement((int)Marshal.OffsetOf(typeof(NkVertex), nameof(NkVertex.Color)), VertexFormat.Color, VertexUsage.Color, 0)
-			});
-		}
-
-		public override void Init() {
-			Wind = Engine.Ultraviolet.GetPlatform().Windows.First();
-			Graphics = Engine.Ultraviolet.GetGraphics();
-
-			Batch = SpriteBatch.Create();
-
-			RenderTarget = RenderTarget2D.Create(Wind.WindowedClientSize.Width, Wind.WindowedClientSize.Height);
-			ColorBuffer = RenderBuffer2D.Create(RenderBufferFormat.Color, RenderTarget.Width, RenderTarget.Height);
-			RenderTarget.Attach(ColorBuffer);
-
-			Sampling = SamplerState.Create();
-			Sampling.Filter = TextureFilter.Point;
-			Sampling.AddressU = Sampling.AddressV = TextureAddressMode.Clamp;
-
-			DepthStencil = DepthStencilState.Create();
-			DepthStencil.StencilEnable = false;
-			DepthStencil.DepthBufferEnable = false;
-
-			Blending = BlendState.AlphaBlend;
-
-			Rasterizing = RasterizerState.Create();
-			Rasterizing.ScissorTestEnable = true;
-			Rasterizing.CullMode = CullMode.None;
-			//Rasterizing.FillMode = FillMode.Wireframe;
-			VPort = new Viewport(0, 0, Wind.ClientSize.Width, Wind.ClientSize.Height);
-
-			Effect = BasicEffect.Create();
-			Effect.World = Matrix.Identity;
-			Effect.View = Matrix.Identity;
-			Effect.Projection = Matrix.CreateOrthographicOffCenter(0, Wind.ClientSize.Width, Wind.ClientSize.Height, 0, 1, -1);
-
-			Effect.VertexColorEnabled = true;
-			Effect.TextureEnabled = true;
-
-			EnsureBufferSize();
-		}
-
-		void EnsureBufferSize(int MinVert = 4096, int MinInd = 4096) {
-			// TODO: Properly dispose
-			bool GeometryDirty = false;
-
-			if ((VertexBuffer != null && VertexBuffer.VertexCount < MinVert) || VertexBuffer == null) {
-				VertexBuffer = DynamicVertexBuffer.Create(NuklearVertexDecl, MinVert);
-				GeometryDirty = true;
-			}
-
-			if ((IndexBuffer != null && IndexBuffer.IndexCount < MinInd) || IndexBuffer == null) {
-				IndexBuffer = DynamicIndexBuffer.Create(IndexBufferElementType.Int16, MinInd);
-				GeometryDirty = true;
-			}
-
-			if (GeometryDirty || GeomStream == null) {
-				GeomStream = GeometryStream.Create();
-				GeomStream.Attach(VertexBuffer);
-				GeomStream.Attach(IndexBuffer);
-			}
-		}
-
-		public override Texture2D CreateTexture(int W, int H, IntPtr Data) {
-			Color[] ColorData = new Color[W * H];
-
-			for (int y = 0; y < H; y++)
-				for (int x = 0; x < W; x++) {
-					NkColor C = ((NkColor*)Data)[y * W + x];
-					ColorData[y * W + x] = new Color(C.R / 255.0f, C.G / 255.0f, C.B / 255.0f, C.A / 255.0f);
-				}
-
-			Surface2D Surf = Surface2D.Create(W, H);
-			Surf.SetData(ColorData);
-			return Surf.CreateTexture();
-		}
-
-		public void BeginBuffering() {
-			Graphics.SetRenderTarget(RenderTarget);
-			Graphics.Clear(Color.Transparent);
-		}
-
-		public override void SetBuffer(NkVertex[] Verts, ushort[] Inds) {
-			EnsureBufferSize(Verts.Length, Inds.Length);
-
-			for (int i = 0; i < Verts.Length; i++) {
-				Verts[i].Position.X = (int)Verts[i].Position.X;
-				Verts[i].Position.Y = (int)Verts[i].Position.Y;
-			}
-
-			IndexBuffer.SetData(Inds);
-			VertexBuffer.SetData(Verts);
-		}
-
-		public override void Render(NkHandle Userdata, Texture2D Texture, NkRect ClipRect, uint Offset, uint Count) {
-			Effect.Texture = Texture;
-
-			foreach (var Pass in Effect.CurrentTechnique.Passes) {
-				Pass.Apply();
-
-				Graphics.SetViewport(VPort);
-				Graphics.SetBlendState(Blending);
-				Graphics.SetRasterizerState(Rasterizing);
-				Graphics.SetDepthStencilState(DepthStencil);
-				Graphics.SetSamplerState(0, Sampling);
-
-				Graphics.SetScissorRectangle((int)ClipRect.X, (int)ClipRect.Y, (int)ClipRect.W, (int)ClipRect.H);
-				Graphics.SetGeometryStream(GeomStream);
-				Graphics.DrawIndexedPrimitives(PrimitiveType.TriangleList, (int)Offset, (int)Count / 3);
-			}
-		}
-
-		public void EndBuffering() {
-			Graphics.SetRenderTarget(null);
-		}
-
-		public void RenderFinal() {
-			Batch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullNone);
-			Batch.Draw(ColorBuffer, Vector2.Zero, Color.White);
-			Batch.End();
-
-			Graphics.UnbindTexture(ColorBuffer);
-		}
-	}
-
 	public class Engine : UltravioletApplication {
 		internal RenderDevice RenderDevice;
+
+		public static bool HUE_FRAME = true;
 
 		static void Main(string[] args) {
 			Console.Title = "libTech";
@@ -222,6 +73,7 @@ namespace libTech {
 		protected override UltravioletContext OnCreatingUltravioletContext() {
 			OpenGLUltravioletConfiguration Cfg = new OpenGLUltravioletConfiguration();
 			Cfg.MinimumOpenGLVersion = new Version(3, 2);
+			Cfg.BackBufferRenderTargetUsage = RenderTargetUsage.PreserveContents;
 
 			Cfg.WindowIsResizable = CVar.GetBool("resizable");
 			Cfg.WindowIsBorderless = CVar.GetBool("borderless");
@@ -242,7 +94,7 @@ namespace libTech {
 			base.OnInitialized();
 
 			IUltravioletPlatform Platform = Ultraviolet.GetPlatform();
-			IUltravioletWindow Window = Platform.Windows.FirstOrDefault();
+			IUltravioletWindow Window = Platform.Windows.GetPrimary();
 
 			if (Window != null) {
 				Window.Caption = "libTech";
@@ -274,6 +126,11 @@ namespace libTech {
 					RenderDevice.OnMouseButton((NuklearEvent.MouseButton)(Button - 1), Pos.X, Pos.Y, false);
 			};
 
+			Keyboard.ButtonPressed += (W, Dev, Button) => {
+				if (Button == Scancode.Escape)
+					HUE_FRAME = true;
+			};
+
 			NuklearAPI.Init(RenderDevice);
 		}
 
@@ -283,6 +140,12 @@ namespace libTech {
 
 		protected override void OnDrawing(UltravioletTime time) {
 			base.OnDrawing(time);
+			if (HUE_FRAME)
+				Console.WriteLine("BEGIN_DRAWING");
+
+			IUltravioletGraphics UVGfx = Ultraviolet.GetGraphics();
+			Gfx.UVGfx = UVGfx;
+			UVGfx.Clear(new Color(70, 90, 190));
 
 			NuklearAPI.Frame(() => {
 				NuklearAPI.Window("Ultraviolet", 100, 100, 200, 200, NkPanelFlags.BorderTitle | NkPanelFlags.MovableScalable | NkPanelFlags.Minimizable, () => {
@@ -295,6 +158,10 @@ namespace libTech {
 						Environment.Exit(0);
 				});
 			});
+
+			if (HUE_FRAME)
+				Console.WriteLine("END_DRAWING");
+			HUE_FRAME = false;
 		}
 	}
 }
