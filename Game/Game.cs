46984482430f4c3c7f154cb31fd9dca6aaf56c0f
Saša Barišić <cartman300@net.hr>
2018-02-08 17:47:06 +0100

    Moved sample code to game dll

--- Game/Game.cs
+++ Game/Game.cs
@@ -5,17 +5,54 @@ using System.Text;
 using System.Threading.Tasks;
 using libTech;
 using libTech.UI;
+using libTech.Graphics;
+using OpenGL;
+using System.Numerics;
 
 namespace Game {
 	public class Game : LibTechGame {
+		ShaderProgram DefaultShader;
+		VertexArray VertexArray;
+
 		public override void Load() {
+			ShaderStage DefaultVert = new ShaderStage(ShaderType.VertexShader);
+			DefaultVert.SetSourceFile("content/shaders/default.vert");
+			DefaultVert.Compile();
 
+			ShaderStage DefaultFrag = new ShaderStage(ShaderType.FragmentShader);
+			DefaultFrag.SetSourceFile("content/shaders/default.frag");
+			DefaultFrag.Compile();
 
+			DefaultShader = new ShaderProgram();
+			DefaultShader.AttachShader(DefaultVert);
+			DefaultShader.AttachShader(DefaultFrag);
+			DefaultShader.Link();
 
+			BufferObject VertexBuffer = new BufferObject();
+			VertexBuffer.SetData(new Vector3[] { new Vector3(0, 0.5f, 0), new Vector3(0.5f, -0.5f, 0), new Vector3(-0.5f, -0.5f, 0) });
+
+			BufferObject ColorBuffer = new BufferObject();
+			ColorBuffer.SetData(new Vector3[] { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) });
+
+			VertexArray = new VertexArray();
+
+			uint PosAttrib = (uint)DefaultShader.GetAttribLocation("Pos");
+			VertexArray.AttribFormat(PosAttrib);
+			VertexArray.AttribBinding(PosAttrib, VertexArray.BindVertexBuffer(VertexBuffer));
+
+			uint ClrAttrib = (uint)DefaultShader.GetAttribLocation("Clr");
+			VertexArray.AttribFormat(ClrAttrib);
+			VertexArray.AttribBinding(ClrAttrib, VertexArray.BindVertexBuffer(ColorBuffer));
+		}
+		
+		public override void Update(float Dt) {
 		}
 
-		public override void Unload() {
+		public override void Draw(float Dt) {
+			Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+			DefaultShader.Bind();
+			VertexArray.Draw(0, 3);
 		}
 	}
 }
--- libTech/Program.cs
+++ libTech/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using libTech.Reflection;
 using libTech.Importer;
 using System.Reflection;
+using System.Numerics;
 using CARP;
 using System.IO;
 
@@ -18,15 +19,18 @@ using libTech.UI;
 using Glfw3;
 using OpenGL;
 using System.Threading;
-using OpenGL.Objects;
-using OpenGL.Objects.Scene;
 using System.Diagnostics;
+using Khronos;
+
+using libTech.Graphics;
 
 namespace libTech {
-	public static class Engine {
+	public unsafe static class Engine {
 		internal static Glfw.Window Window;
 
 		static int FPS;
+
+		public static LibTechGame Game;
 
 		static void Main(string[] args) {
 			CVar.InitMode = true;
@@ -36,6 +40,12 @@ namespace libTech {
 			CVar.Register("height", 600, CVarType.Archive);
 			CVar.Register("borderless", false, CVarType.Archive);
 			CVar.Register("resizable", false, CVarType.Archive);
+			CVar.Register("gl_doublebuffer", true, CVarType.Archive);
+			CVar.Register("gl_samples", 0, CVarType.Archive);
+
+			CVar.Register("gl_forwardcompat", true, CVarType.Archive | CVarType.Init | CVarType.Unsafe);
+			CVar.Register("gl_major", 4, CVarType.Archive | CVarType.Init | CVarType.Unsafe);
+			CVar.Register("gl_minor", 5, CVarType.Archive | CVarType.Init | CVarType.Unsafe);
 
 			// Parse all arguments and set CVars
 			foreach (var Arg in ArgumentParser.All) {
@@ -61,9 +71,7 @@ namespace libTech {
 			foreach (var CVar in CVar.GetAll())
 				GConsole.WriteLine(CVar);
 
-			foreach (var Type in Reflect.GetAllTypes(Reflect.GetExeAssembly()))
-				if (!Type.IsAbstract && Reflect.Inherits(Type, typeof(Importer.Importer)))
-					Importers.Register(Type);
+			Importers.RegisterAll(Reflect.GetExeAssembly());
 
 			CreateContext();
 			LoadContent();
@@ -73,11 +81,16 @@ namespace libTech {
 			float Dt = Target;
 
 			while (!Glfw.WindowShouldClose(Window)) {
-				Update(Dt);
-				Draw(Dt);
+				Glfw.PollEvents();
+				Game.Update(Dt);
 
+				Game.Draw(Dt);
+				Glfw.SwapBuffers(Window);
+				
+				// Cap at Target framerate
 				while ((float)SWatch.ElapsedMilliseconds / 1000 < Target)
 					;
+
 				Dt = (float)SWatch.ElapsedMilliseconds / 1000;
 				FPS = (int)(1.0f / Dt);
 				SWatch.Restart();
@@ -86,34 +99,104 @@ namespace libTech {
 			Environment.Exit(0);
 		}
 
-		static void CreateContext() {
+		static void FatalError(string Msg) {
+			GConsole.WriteLine(Msg);
+
+			while (true)
+				Thread.Sleep(10);
+		}
+
+		static void FatalError(string Fmt, params object[] Args) {
+			FatalError(string.Format(Fmt, Args));
+		}
+
+		static void CreateContext(/*params string[] RequiredExtensions*/) {
+			GConsole.WriteLine("Initializing OpenGL");
 			Gl.Initialize();
-			Glfw.ConfigureNativesDirectory("native/glfw3_64"); // TODO
 
-			if (!Glfw.Init()) {
-				GConsole.WriteLine("Could not initialize GLFW");
-
-				while (true)
-					Thread.Sleep(10);
-			}
+			GConsole.WriteLine("Initializing GLFW");
+			Glfw.ConfigureNativesDirectory("native/glfw3_64");
+			if (!Glfw.Init())
+				FatalError("Could not initialize GLFW");
 
 			int W = CVar.GetInt("width", 800);
 			int H = CVar.GetInt("height", 600);
 
-			Window = Glfw.CreateWindow(W, H, "libTech");
-			if (!Window) {
-				GConsole.WriteLine("Could not create window({0}x{1})", W, H);
+			Glfw.WindowHint(Glfw.Hint.Resizable, CVar.GetBool("resizable"));
 
-				while (true)
-					Thread.Sleep(10);
-			}
+			//Glfw.WindowHint(Glfw.Hint.ClientApi, Glfw.ClientApi.None);
+			Glfw.WindowHint(Glfw.Hint.ClientApi, Glfw.ClientApi.OpenGL);
+			Glfw.WindowHint(Glfw.Hint.ContextCreationApi, Glfw.ContextApi.Native);
+			Glfw.WindowHint(Glfw.Hint.Doublebuffer, CVar.GetBool("gl_doublebuffer"));
+			Glfw.WindowHint(Glfw.Hint.ContextVersionMajor, CVar.GetInt("gl_major"));
+			Glfw.WindowHint(Glfw.Hint.ContextVersionMinor, CVar.GetInt("gl_minor"));
+
+			Glfw.WindowHint(Glfw.Hint.Samples, CVar.GetInt("gl_samples"));
+			Glfw.WindowHint(Glfw.Hint.OpenglForwardCompat, CVar.GetBool("gl_forwardcompat"));
+			Glfw.WindowHint(Glfw.Hint.OpenglProfile, Glfw.OpenGLProfile.Core);
+#if DEBUG
+			Glfw.WindowHint(Glfw.Hint.OpenglDebugContext, true);
+#endif
+
+			GConsole.WriteLine("Creating window");
+			Window = Glfw.CreateWindow(W, H, "libTech");
+			if (!Window)
+				FatalError("Could not create window({0}x{1})", W, H);
 
 			Glfw.MakeContextCurrent(Window);
+
+			/*bool AllSupported = true;
+
+			for (int i = 0; i < RequiredExtensions.Length; i++) {
+				if (!Glfw.ExtensionSupported(RequiredExtensions[i])) {
+					GConsole.WriteLine("{0} not supported", RequiredExtensions[i]);
+					AllSupported = false;
+				}
+			}
+
+			if (!AllSupported)
+				while (true)
+					Thread.Sleep(10);*/
+
+#if DEBUG
+			Gl.DebugMessageCallback((Src, DbgType, ID, Severity, Len, Buffer, UserPtr) => {
+				if (Severity == Gl.DebugSeverity.Notification)
+					return;
+
+				GConsole.WriteLine("OpenGL {0} {1} {2}, {3}", Src, DbgType, ID, Severity);
+				GConsole.WriteLine(Encoding.ASCII.GetString((byte*)Buffer, Len));
+
+				if ((Severity == Gl.DebugSeverity.Medium || Severity == Gl.DebugSeverity.High) && Debugger.IsAttached)
+					Debugger.Break();
+			}, IntPtr.Zero);
+#endif
+
+			GConsole.WriteLine("{0}, {1}", Gl.GetString(StringName.Vendor), Gl.GetString(StringName.Renderer));
+			GConsole.WriteLine("OpenGL {0}", Gl.GetString(StringName.Version));
+			GConsole.WriteLine("GLSL {0}", Gl.GetString(StringName.ShadingLanguageVersion));
 
 			Gl.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		}
 
 		static void LoadContent() {
+			string GameDllPath = Path.Combine(CVar.GetString("game"), "Game.dll");
+
+			if (!File.Exists(GameDllPath))
+				FatalError("File not found: {0}", GameDllPath);
+
+			Assembly GameAssembly = Reflect.LoadAssembly(GameDllPath);
+			Importers.RegisterAll(GameAssembly);
+
+			Type[] GameImplementations = Reflect.GetAllImplementationsOf(GameAssembly, typeof(LibTechGame)).ToArray();
+
+			if (GameImplementations.Length == 0)
+				FatalError("Could not find game implementation in {0}", GameDllPath);
+			if (GameImplementations.Length > 1)
+				FatalError("Found too many game implementations in {0}", GameDllPath);
+
+			Game = (LibTechGame)Activator.CreateInstance(GameImplementations[0]);
+			Game.Load();
+
 			/*base.OnLoadingContent();
 
 			IUltravioletPlatform Platform = Ultraviolet.GetPlatform();
@@ -181,28 +264,6 @@ namespace libTech {
 			};
 
 			NuklearAPI.Init(RenderDevice);*/
-		}
-
-		static void Update(float Time) {
-			Glfw.PollEvents();
-
-
-		}
-
-		static void Draw(float Time) {
-			Gl.Clear(ClearBufferMask.ColorBufferBit);
-
-			/*base.OnDrawing(time);
-
-			IUltravioletGraphics UVGfx = Ultraviolet.GetGraphics();
-			Gfx.UVGfx = UVGfx;
-			UVGfx.Clear(new Color(70, 90, 190));
-
-			NuklearAPI.Frame(() => {
-				GConsole.NuklearDraw(100, 100);
-			});*/
-
-			Glfw.SwapBuffers(Window);
 		}
 	}
 }
