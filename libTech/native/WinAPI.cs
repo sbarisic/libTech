21b56cdae13f9855986f2f71827f488c93b33924
Saša Barišić <cartman300@net.hr>
2018-02-08 17:44:51 +0100

    Placeholder WinAPI

---
+++ libTech/native/WinAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libTech {
+	/*internal static class User32 {
+		const string DllName = "User32";
+		const CallingConvention CConv = CallingConvention.Cdecl;
+
+		[DllImport(DllName, CallingConvention = CConv)]
+		public static extern IntPtr WindowFromDC(IntPtr DC);
+	}*/
+}
