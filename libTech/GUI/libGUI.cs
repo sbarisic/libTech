266da2289f4ef46093c325670486b10745d3c8ae
Saša Barišić <cartman300@net.hr>
2019-01-09 09:30:00 +0100

    Skinning

--- libTech/GUI/libGUI.cs
+++ libTech/GUI/libGUI.cs
@@ -66,12 +66,38 @@ namespace libTech.GUI {
 		}
 	}
 
+	struct Media {
+		public int skin;
+
+		public nk_image menu;
+		public nk_image check;
+		public nk_image check_cursor;
+		public nk_image option;
+		public nk_image option_cursor;
+		public nk_image header;
+		public nk_image window;
+		public nk_image scrollbar_inc_button;
+		public nk_image scrollbar_inc_button_hover;
+		public nk_image scrollbar_dec_button;
+		public nk_image scrollbar_dec_button_hover;
+		public nk_image button;
+		public nk_image button_hover;
+		public nk_image button_active;
+		public nk_image tab_minimize;
+		public nk_image tab_maximize;
+		public nk_image slider;
+		public nk_image slider_hover;
+		public nk_image slider_active;
+	}
+
 	public unsafe class libGUI {
 		FishGfxDevice Dev;
 		RenderWindow RWind;
 		List<FMLDocument> Docs;
 
 		int FreeWindowSlot;
+
+		Media media;
 
 		public void Init(RenderWindow RWind, ShaderProgram GUIShader) {
 			this.RWind = RWind;
@@ -83,7 +109,308 @@ namespace libTech.GUI {
 			NuklearAPI.Init(Dev);
 
 			// TODO: Skinning
-			//NuklearAPI.Ctx-> 
+			ref nk_style style = ref NuklearAPI.Ctx->style;
+			Texture Tex = Texture.FromFile("content/textures/gwen.png");
+			Tex.SetFilter(TextureFilter.Linear);
+
+			media = new Media();
+			media.skin = Dev.CreateTextureHandle(Tex);
+			media.check = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(464, 32, 15, 15));
+			media.check_cursor = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(450, 34, 11, 11));
+			media.option = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(464, 64, 15, 15));
+			media.option_cursor = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(451, 67, 9, 9));
+			media.header = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(128, 0, 127, 24));
+			media.window = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(128, 23, 127, 104));
+			media.scrollbar_inc_button = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(464, 256, 15, 15));
+			media.scrollbar_inc_button_hover = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(464, 320, 15, 15));
+			media.scrollbar_dec_button = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(464, 224, 15, 15));
+			media.scrollbar_dec_button_hover = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(464, 288, 15, 15));
+			media.button = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(384, 336, 127, 31));
+			media.button_hover = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(384, 368, 127, 31));
+			media.button_active = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(384, 400, 127, 31));
+			media.tab_minimize = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(451, 99, 9, 9));
+			media.tab_maximize = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(467, 99, 9, 9));
+			media.slider = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(418, 33, 11, 14));
+			media.slider_hover = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(418, 49, 11, 14));
+			media.slider_active = Nuklear.nk_subimage_id(media.skin, 512, 512, new NkRect(418, 64, 11, 14));
+
+			style.window.background = new NkColor(204, 204, 204);
+			style.window.fixed_background = Nuklear.nk_style_item_image(media.window);
+			style.window.border_color = new NkColor(67, 67, 67);
+			style.window.combo_border_color = new NkColor(67, 67, 67);
+			style.window.contextual_border_color = new NkColor(67, 67, 67);
+			style.window.menu_border_color = new NkColor(67, 67, 67);
+			style.window.group_border_color = new NkColor(67, 67, 67);
+			style.window.tooltip_border_color = new NkColor(67, 67, 67);
+			style.window.scrollbar_size = new nk_vec2(16, 16);
+			style.window.border_color = new NkColor(0, 0, 0, 0);
+			style.window.padding = new nk_vec2(8, 4);
+			style.window.border = 3;
+
+			/* window header */
+			style.window.header.normal = Nuklear.nk_style_item_image(media.header);
+			style.window.header.hover = Nuklear.nk_style_item_image(media.header);
+			style.window.header.active = Nuklear.nk_style_item_image(media.header);
+			style.window.header.label_normal = new NkColor(95, 95, 95);
+			style.window.header.label_hover = new NkColor(95, 95, 95);
+			style.window.header.label_active = new NkColor(95, 95, 95);
+
+			/* scrollbar */
+			style.scrollv.normal = Nuklear.nk_style_item_color(new NkColor(184, 184, 184));
+			style.scrollv.hover = Nuklear.nk_style_item_color(new NkColor(184, 184, 184));
+			style.scrollv.active = Nuklear.nk_style_item_color(new NkColor(184, 184, 184));
+			style.scrollv.cursor_normal = Nuklear.nk_style_item_color(new NkColor(220, 220, 220));
+			style.scrollv.cursor_hover = Nuklear.nk_style_item_color(new NkColor(235, 235, 235));
+			style.scrollv.cursor_active = Nuklear.nk_style_item_color(new NkColor(99, 202, 255));
+			style.scrollv.dec_symbol = nk_symbol_type.NK_SYMBOL_NONE;
+			style.scrollv.inc_symbol = nk_symbol_type.NK_SYMBOL_NONE;
+			style.scrollv.show_buttons = 1;
+			style.scrollv.border_color = new NkColor(81, 81, 81);
+			style.scrollv.cursor_border_color = new NkColor(81, 81, 81);
+			style.scrollv.border = 1;
+			style.scrollv.rounding = 0;
+			style.scrollv.border_cursor = 1;
+			style.scrollv.rounding_cursor = 2;
+
+			/* scrollbar buttons */
+			style.scrollv.inc_button.normal = Nuklear.nk_style_item_image(media.scrollbar_inc_button);
+			style.scrollv.inc_button.hover = Nuklear.nk_style_item_image(media.scrollbar_inc_button_hover);
+			style.scrollv.inc_button.active = Nuklear.nk_style_item_image(media.scrollbar_inc_button_hover);
+			style.scrollv.inc_button.border_color = new NkColor(0, 0, 0, 0);
+			style.scrollv.inc_button.text_background = new NkColor(0, 0, 0, 0);
+			style.scrollv.inc_button.text_normal = new NkColor(0, 0, 0, 0);
+			style.scrollv.inc_button.text_hover = new NkColor(0, 0, 0, 0);
+			style.scrollv.inc_button.text_active = new NkColor(0, 0, 0, 0);
+			style.scrollv.inc_button.border = 0.0f;
+
+			style.scrollv.dec_button.normal = Nuklear.nk_style_item_image(media.scrollbar_dec_button);
+			style.scrollv.dec_button.hover = Nuklear.nk_style_item_image(media.scrollbar_dec_button_hover);
+			style.scrollv.dec_button.active = Nuklear.nk_style_item_image(media.scrollbar_dec_button_hover);
+			style.scrollv.dec_button.border_color = new NkColor(0, 0, 0, 0);
+			style.scrollv.dec_button.text_background = new NkColor(0, 0, 0, 0);
+			style.scrollv.dec_button.text_normal = new NkColor(0, 0, 0, 0);
+			style.scrollv.dec_button.text_hover = new NkColor(0, 0, 0, 0);
+			style.scrollv.dec_button.text_active = new NkColor(0, 0, 0, 0);
+			style.scrollv.dec_button.border = 0.0f;
+
+			/* checkbox toggle */
+			{
+				ref nk_style_toggle toggle = ref style.checkbox;
+				toggle.normal = Nuklear.nk_style_item_image(media.check);
+				toggle.hover = Nuklear.nk_style_item_image(media.check);
+				toggle.active = Nuklear.nk_style_item_image(media.check);
+				toggle.cursor_normal = Nuklear.nk_style_item_image(media.check_cursor);
+				toggle.cursor_hover = Nuklear.nk_style_item_image(media.check_cursor);
+				toggle.text_normal = new NkColor(95, 95, 95);
+				toggle.text_hover = new NkColor(95, 95, 95);
+				toggle.text_active = new NkColor(95, 95, 95);
+			}
+
+			/* option toggle */
+			{
+				ref nk_style_toggle toggle = ref style.option;
+				toggle.normal = Nuklear.nk_style_item_image(media.option);
+				toggle.hover = Nuklear.nk_style_item_image(media.option);
+				toggle.active = Nuklear.nk_style_item_image(media.option);
+				toggle.cursor_normal = Nuklear.nk_style_item_image(media.option_cursor);
+				toggle.cursor_hover = Nuklear.nk_style_item_image(media.option_cursor);
+				toggle.text_normal = new NkColor(95, 95, 95);
+				toggle.text_hover = new NkColor(95, 95, 95);
+				toggle.text_active = new NkColor(95, 95, 95);
+			}
+
+			/* default button */
+			style.button.normal = Nuklear.nk_style_item_image(media.button);
+			style.button.hover = Nuklear.nk_style_item_image(media.button_hover);
+			style.button.active = Nuklear.nk_style_item_image(media.button_active);
+			style.button.border_color = new NkColor(0, 0, 0, 0);
+			style.button.text_background = new NkColor(0, 0, 0, 0);
+			style.button.text_normal = new NkColor(95, 95, 95);
+			style.button.text_hover = new NkColor(95, 95, 95);
+			style.button.text_active = new NkColor(95, 95, 95);
+
+			/* default text */
+			style.text.color = new NkColor(95, 95, 95);
+
+			/* contextual button */
+			style.contextual_button.normal = Nuklear.nk_style_item_color(new NkColor(206, 206, 206));
+			style.contextual_button.hover = Nuklear.nk_style_item_color(new NkColor(229, 229, 229));
+			style.contextual_button.active = Nuklear.nk_style_item_color(new NkColor(99, 202, 255));
+			style.contextual_button.border_color = new NkColor(0, 0, 0, 0);
+			style.contextual_button.text_background = new NkColor(0, 0, 0, 0);
+			style.contextual_button.text_normal = new NkColor(95, 95, 95);
+			style.contextual_button.text_hover = new NkColor(95, 95, 95);
+			style.contextual_button.text_active = new NkColor(95, 95, 95);
+
+			/* menu button */
+			style.menu_button.normal = Nuklear.nk_style_item_color(new NkColor(206, 206, 206));
+			style.menu_button.hover = Nuklear.nk_style_item_color(new NkColor(229, 229, 229));
+			style.menu_button.active = Nuklear.nk_style_item_color(new NkColor(99, 202, 255));
+			style.menu_button.border_color = new NkColor(0, 0, 0, 0);
+			style.menu_button.text_background = new NkColor(0, 0, 0, 0);
+			style.menu_button.text_normal = new NkColor(95, 95, 95);
+			style.menu_button.text_hover = new NkColor(95, 95, 95);
+			style.menu_button.text_active = new NkColor(95, 95, 95);
+
+			/* tree */
+			style.tab.text = new NkColor(95, 95, 95);
+			style.tab.tab_minimize_button.normal = Nuklear.nk_style_item_image(media.tab_minimize);
+			style.tab.tab_minimize_button.hover = Nuklear.nk_style_item_image(media.tab_minimize);
+			style.tab.tab_minimize_button.active = Nuklear.nk_style_item_image(media.tab_minimize);
+			style.tab.tab_minimize_button.text_background = new NkColor(0, 0, 0, 0);
+			style.tab.tab_minimize_button.text_normal = new NkColor(0, 0, 0, 0);
+			style.tab.tab_minimize_button.text_hover = new NkColor(0, 0, 0, 0);
+			style.tab.tab_minimize_button.text_active = new NkColor(0, 0, 0, 0);
+
+			style.tab.tab_maximize_button.normal = Nuklear.nk_style_item_image(media.tab_maximize);
+			style.tab.tab_maximize_button.hover = Nuklear.nk_style_item_image(media.tab_maximize);
+			style.tab.tab_maximize_button.active = Nuklear.nk_style_item_image(media.tab_maximize);
+			style.tab.tab_maximize_button.text_background = new NkColor(0, 0, 0, 0);
+			style.tab.tab_maximize_button.text_normal = new NkColor(0, 0, 0, 0);
+			style.tab.tab_maximize_button.text_hover = new NkColor(0, 0, 0, 0);
+			style.tab.tab_maximize_button.text_active = new NkColor(0, 0, 0, 0);
+
+			style.tab.node_minimize_button.normal = Nuklear.nk_style_item_image(media.tab_minimize);
+			style.tab.node_minimize_button.hover = Nuklear.nk_style_item_image(media.tab_minimize);
+			style.tab.node_minimize_button.active = Nuklear.nk_style_item_image(media.tab_minimize);
+			style.tab.node_minimize_button.text_background = new NkColor(0, 0, 0, 0);
+			style.tab.node_minimize_button.text_normal = new NkColor(0, 0, 0, 0);
+			style.tab.node_minimize_button.text_hover = new NkColor(0, 0, 0, 0);
+			style.tab.node_minimize_button.text_active = new NkColor(0, 0, 0, 0);
+
+			style.tab.node_maximize_button.normal = Nuklear.nk_style_item_image(media.tab_maximize);
+			style.tab.node_maximize_button.hover = Nuklear.nk_style_item_image(media.tab_maximize);
+			style.tab.node_maximize_button.active = Nuklear.nk_style_item_image(media.tab_maximize);
+			style.tab.node_maximize_button.text_background = new NkColor(0, 0, 0, 0);
+			style.tab.node_maximize_button.text_normal = new NkColor(0, 0, 0, 0);
+			style.tab.node_maximize_button.text_hover = new NkColor(0, 0, 0, 0);
+			style.tab.node_maximize_button.text_active = new NkColor(0, 0, 0, 0);
+
+			/* selectable */
+			style.selectable.normal = Nuklear.nk_style_item_color(new NkColor(206, 206, 206));
+			style.selectable.hover = Nuklear.nk_style_item_color(new NkColor(206, 206, 206));
+			style.selectable.pressed = Nuklear.nk_style_item_color(new NkColor(206, 206, 206));
+			style.selectable.normal_active = Nuklear.nk_style_item_color(new NkColor(185, 205, 248));
+			style.selectable.hover_active = Nuklear.nk_style_item_color(new NkColor(185, 205, 248));
+			style.selectable.pressed_active = Nuklear.nk_style_item_color(new NkColor(185, 205, 248));
+			style.selectable.text_normal = new NkColor(95, 95, 95);
+			style.selectable.text_hover = new NkColor(95, 95, 95);
+			style.selectable.text_pressed = new NkColor(95, 95, 95);
+			style.selectable.text_normal_active = new NkColor(95, 95, 95);
+			style.selectable.text_hover_active = new NkColor(95, 95, 95);
+			style.selectable.text_pressed_active = new NkColor(95, 95, 95);
+
+			/* slider */
+			style.slider.normal = Nuklear.nk_style_item_hide();
+			style.slider.hover = Nuklear.nk_style_item_hide();
+			style.slider.active = Nuklear.nk_style_item_hide();
+			style.slider.bar_normal = new NkColor(156, 156, 156);
+			style.slider.bar_hover = new NkColor(156, 156, 156);
+			style.slider.bar_active = new NkColor(156, 156, 156);
+			style.slider.bar_filled = new NkColor(156, 156, 156);
+			style.slider.cursor_normal = Nuklear.nk_style_item_image(media.slider);
+			style.slider.cursor_hover = Nuklear.nk_style_item_image(media.slider_hover);
+			style.slider.cursor_active = Nuklear.nk_style_item_image(media.slider_active);
+			style.slider.cursor_size = new nk_vec2(16.5f, 21);
+			style.slider.bar_height = 1;
+
+			/* progressbar */
+			style.progress.normal = Nuklear.nk_style_item_color(new NkColor(231, 231, 231));
+			style.progress.hover = Nuklear.nk_style_item_color(new NkColor(231, 231, 231));
+			style.progress.active = Nuklear.nk_style_item_color(new NkColor(231, 231, 231));
+			style.progress.cursor_normal = Nuklear.nk_style_item_color(new NkColor(63, 242, 93));
+			style.progress.cursor_hover = Nuklear.nk_style_item_color(new NkColor(63, 242, 93));
+			style.progress.cursor_active = Nuklear.nk_style_item_color(new NkColor(63, 242, 93));
+			style.progress.border_color = new NkColor(114, 116, 115);
+			style.progress.padding = new nk_vec2(0, 0);
+			style.progress.border = 2;
+			style.progress.rounding = 1;
+
+			/* combo */
+			style.combo.normal = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.combo.hover = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.combo.active = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.combo.border_color = new NkColor(95, 95, 95);
+			style.combo.label_normal = new NkColor(95, 95, 95);
+			style.combo.label_hover = new NkColor(95, 95, 95);
+			style.combo.label_active = new NkColor(95, 95, 95);
+			style.combo.border = 1;
+			style.combo.rounding = 1;
+
+			/* combo button */
+			style.combo.button.normal = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.combo.button.hover = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.combo.button.active = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.combo.button.text_background = new NkColor(216, 216, 216);
+			style.combo.button.text_normal = new NkColor(95, 95, 95);
+			style.combo.button.text_hover = new NkColor(95, 95, 95);
+			style.combo.button.text_active = new NkColor(95, 95, 95);
+
+			/* property */
+			style.property.normal = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.hover = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.active = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.border_color = new NkColor(81, 81, 81);
+			style.property.label_normal = new NkColor(95, 95, 95);
+			style.property.label_hover = new NkColor(95, 95, 95);
+			style.property.label_active = new NkColor(95, 95, 95);
+			style.property.sym_left = nk_symbol_type.NK_SYMBOL_TRIANGLE_LEFT;
+			style.property.sym_right = nk_symbol_type.NK_SYMBOL_TRIANGLE_RIGHT;
+			style.property.rounding = 10;
+			style.property.border = 1;
+
+			/* edit */
+			style.edit.normal = Nuklear.nk_style_item_color(new NkColor(240, 240, 240));
+			style.edit.hover = Nuklear.nk_style_item_color(new NkColor(240, 240, 240));
+			style.edit.active = Nuklear.nk_style_item_color(new NkColor(240, 240, 240));
+			style.edit.border_color = new NkColor(62, 62, 62);
+			style.edit.cursor_normal = new NkColor(99, 202, 255);
+			style.edit.cursor_hover = new NkColor(99, 202, 255);
+			style.edit.cursor_text_normal = new NkColor(95, 95, 95);
+			style.edit.cursor_text_hover = new NkColor(95, 95, 95);
+			style.edit.text_normal = new NkColor(95, 95, 95);
+			style.edit.text_hover = new NkColor(95, 95, 95);
+			style.edit.text_active = new NkColor(95, 95, 95);
+			style.edit.selected_normal = new NkColor(99, 202, 255);
+			style.edit.selected_hover = new NkColor(99, 202, 255);
+			style.edit.selected_text_normal = new NkColor(95, 95, 95);
+			style.edit.selected_text_hover = new NkColor(95, 95, 95);
+			style.edit.border = 1;
+			style.edit.rounding = 2;
+
+			/* property buttons */
+			style.property.dec_button.normal = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.dec_button.hover = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.dec_button.active = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.dec_button.text_background = new NkColor(0, 0, 0, 0);
+			style.property.dec_button.text_normal = new NkColor(95, 95, 95);
+			style.property.dec_button.text_hover = new NkColor(95, 95, 95);
+			style.property.dec_button.text_active = new NkColor(95, 95, 95);
+			style.property.inc_button = style.property.dec_button;
+
+			/* property edit */
+			style.property.edit.normal = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.edit.hover = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.edit.active = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.property.edit.border_color = new NkColor(0, 0, 0, 0);
+			style.property.edit.cursor_normal = new NkColor(95, 95, 95);
+			style.property.edit.cursor_hover = new NkColor(95, 95, 95);
+			style.property.edit.cursor_text_normal = new NkColor(216, 216, 216);
+			style.property.edit.cursor_text_hover = new NkColor(216, 216, 216);
+			style.property.edit.text_normal = new NkColor(95, 95, 95);
+			style.property.edit.text_hover = new NkColor(95, 95, 95);
+			style.property.edit.text_active = new NkColor(95, 95, 95);
+			style.property.edit.selected_normal = new NkColor(95, 95, 95);
+			style.property.edit.selected_hover = new NkColor(95, 95, 95);
+			style.property.edit.selected_text_normal = new NkColor(216, 216, 216);
+			style.property.edit.selected_text_hover = new NkColor(216, 216, 216);
+
+			/* chart */
+			style.chart.background = Nuklear.nk_style_item_color(new NkColor(216, 216, 216));
+			style.chart.border_color = new NkColor(81, 81, 81);
+			style.chart.color = new NkColor(95, 95, 95);
+			style.chart.selected_color = new NkColor(255, 0, 0);
+			style.chart.border = 1;
 		}
 
 		public void OnMouseMove(RenderWindow RWind, float X, float Y) {
