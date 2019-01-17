acd4041d6b0570bc28dc313b700ad53b7a390b72
Saša Barišić <cartman300@net.hr>
2019-01-17 15:56:41 +0100

    Added new main menu UI

--- Game/BaseGame.cs
+++ Game/BaseGame.cs
@@ -6,6 +6,7 @@ using libTech;
 using libTech.Entities;
 using libTech.Graphics;
 using libTech.GUI;
+using libTech.GUI.Controls;
 using libTech.Importer;
 using libTech.libNative;
 using libTech.Scripting;
@@ -23,6 +24,8 @@ using Color = FishGfx.Color;
 
 namespace Game {
 	public unsafe class Game : LibTechGame {
+		bool OptionsWindowShown = false;
+
 		ShaderProgram MenuMeshShader;
 		Mesh3D MenuMesh;
 		Texture MenuMeshTex;
@@ -36,8 +39,37 @@ namespace Game {
 			Camera Cam = Engine.Camera3D;
 			Cam.Position = new Vector3(40, 25, 40);
 			Cam.LookAt(Vector3.Zero);
+
+			Window MainMenuWindow = new Window(Engine.UI);
+			MainMenuWindow.Position = new Vector2(50, 50);
+			MainMenuWindow.SizeMode = SizeMode.EncapsuleChildren;
+			MainMenuWindow.Title = "libTech";
+			MainMenuWindow.Closable = false;
+			Engine.UI.AddControl(MainMenuWindow);
+
+			const float BtnPadding = 7;
+			const float BtnHeight = 30;
+			const float BtnWidth = 200;
+
+			MainMenuWindow.AddButton(MainMenuWindow.ClientArea + new Vector2(0, BtnHeight * 2 + BtnPadding * 2), new Vector2(BtnWidth, BtnHeight), "New Game", (S, E) => { });
+			MainMenuWindow.AddButton(MainMenuWindow.ClientArea + new Vector2(0, BtnHeight * 1 + BtnPadding * 1), new Vector2(BtnWidth, BtnHeight), "Options", (S, E) => { SpawnOptionsWindow(); });
+			MainMenuWindow.AddButton(MainMenuWindow.ClientArea + new Vector2(0, BtnHeight * 0 + BtnPadding * 0), new Vector2(BtnWidth, BtnHeight), "Quit", (S, E) => { Environment.Exit(0); });
 		}
-		
+
+		void SpawnOptionsWindow() {
+			if (OptionsWindowShown)
+				return;
+
+			OptionsWindowShown = true;
+
+			Window OptionsWindow = new Window(Engine.UI);
+			OptionsWindow.OnClose += () => OptionsWindowShown = false;
+			OptionsWindow.Title = "Options";
+			OptionsWindow.Position = new Vector2(200, 200);
+			OptionsWindow.Size = new Vector2(300, 300);
+			Engine.UI.AddControl(OptionsWindow);
+		}
+
 		public override void Draw(float Dt) {
 			base.Draw(Dt);
 
